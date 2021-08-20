5a21916675e7ed509d6b96066412b9579b7d5b7f
Ga-yo <39812@naver.com>
2021-08-20 23:24:25 +0900

    ADD : Template

---
+++ Rx+MVVM.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    //MARK: UI
+    
+    //MARK: Properties
+    
+    //MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: Binding
+    private func bindViewModel() {
+        
+    }
+}
---
+++ Rx+MVVM.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+
+import Foundation
+
+final class ___FILEBASENAMEASIDENTIFIER___ViewModel: ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+    }
+}
